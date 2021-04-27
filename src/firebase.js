48051c68ec6a36f80a3ea2aa7171a4438e2213d9
Gitene <12866tng@gmail.com>
2021-04-27 11:32:58 +0300

    Day one work. Did navbar, home pageand started working on the check out page.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzhPby7Ltr0AZJfA52LjHVn8DBWmSPzAg",
+    authDomain: "ecommerce-5770d.firebaseapp.com",
+    projectId: "ecommerce-5770d",
+    storageBucket: "ecommerce-5770d.appspot.com",
+    messagingSenderId: "754702104420",
+    appId: "1:754702104420:web:b93a9ee49c717ebb7b6c71",
+    measurementId: "G-QP6QZ1W13X"
+  };
